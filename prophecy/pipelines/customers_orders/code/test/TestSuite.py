1157478b7ffd503b0b03896c969eb05c9813da39
scottalyons@gmail.com <scottalyons@gmail.com>
2023-02-10 06:02:53 +0000

    Adding dummy unit test

--- prophecy/pipelines/customers_orders/code/test/TestSuite.py
+++ prophecy/pipelines/customers_orders/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.job.graph.test_By_CustomerId import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
---
+++ prophecy/pipelines/customers_orders/code/test/job/graph/__init__.py
@@ -0,0 +1,1 @@
+from .test_By_CustomerId import *
