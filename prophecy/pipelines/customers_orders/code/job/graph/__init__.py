4f4dc7d9dbb2c5150d0921abedffbd343a5ac073
scottalyons@gmail.com <scottalyons@gmail.com>
2023-03-27 15:29:48 +0000

    🔁 Modified     - Pipeline        - customers_orders

--- prophecy/pipelines/customers_orders/code/job/graph/__init__.py
+++ prophecy/pipelines/customers_orders/code/job/graph/__init__.py
@@ -1,4 +1,6 @@
+from .orders import orders
 from .Sum_Amounts import Sum_Amounts
 from .Cleanup import Cleanup
+from .customers import customers
 from .By_CustomerId import By_CustomerId
 from .Customer_Orders import Customer_Orders
