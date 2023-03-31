24b3ab4e498d7d0c03eb1e01f396db3a355f275e
scottalyons@gmail.com <scottalyons@gmail.com>
2023-03-31 19:21:41 +0000

    Reverted to Reset point, commit hash : 7132aea2c606ac075b1e9e1796481b8aa806c39b

--- prophecy/pipelines/customers_orders/code/job/graph/__init__.py
+++ prophecy/pipelines/customers_orders/code/job/graph/__init__.py
@@ -1,6 +1,4 @@
-from .orders import orders
 from .Sum_Amounts import Sum_Amounts
 from .Cleanup import Cleanup
-from .customers import customers
 from .By_CustomerId import By_CustomerId
 from .Customer_Orders import Customer_Orders
