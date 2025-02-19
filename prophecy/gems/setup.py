a2c61f70a7d642ccc06736d50a1d54eb4e70e471
RJ <richard@prophecy.io>
2025-02-18 18:59:20 -0600

    adding branch for big version test

--- prophecy/gems/setup.py
+++ prophecy/gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'scott_demo2_team_helloworld',
-    version = '0.7.0',
+    version = '9999.0.0',
     packages = packages_to_include,
     description = '',
     install_requires = [],
